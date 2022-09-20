0f60135ed5017545df39016fc29092555d847f21
Sumitha <sumithamg28@gmail.com>
2022-09-20 17:36:12 +0100

    Revert "Initial commit"
    This reverts commit 35ef1d80ec0decd9d8c96f1c697294d0e766a226.

--- src/main/java/com/example/demo/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Orders, Integer> {
-
-}
